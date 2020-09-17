031f42008b2ae0fda48addab8398f4c223e4c244
Usama Shehzad <usamashzd99@gmail.com>
2020-09-17 15:35:36 +0500

    html conflict between server and client fixed

--- client/routes.js
+++ client/routes.js
@@ -1,29 +1,27 @@
-import loadable from '@loadable/component'
+import loadable from "@loadable/component";
 
 import App from "./App";
 
-const Home = loadable(() => import("./pages/Home"), { ssr: true })
-const About = loadable(() => import("./pages/About"), { ssr: true })
-const NotFound = loadable(() => import("./pages/NotFound"), { ssr: true })
-
+const Home = loadable(() => import("./pages/Home"), { ssr: false });
+const About = loadable(() => import("./pages/About"), { ssr: false });
+const NotFound = loadable(() => import("./pages/NotFound"), { ssr: false });
 
 export default [
-    {
-        component: App,
-        routes: [
-            {
-                path: '/about',
-                component: About,
-            },
-            {
-                path: '/',
-                exact: true,
-                component: Home
-            },
-            {
-                component: NotFound
-            }
-        ]
-    }
+  {
+    component: App,
+    routes: [
+      {
+        path: "/about",
+        component: About,
+      },
+      {
+        path: "/",
+        exact: true,
+        component: Home,
+      },
+      {
+        component: NotFound,
+      },
+    ],
+  },
 ];
-
