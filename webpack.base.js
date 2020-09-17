11f63e84d547cf4c2f714fe5877f6b97f96b50c6
Usama Shehzad <usamashzd99@gmail.com>
2020-09-17 13:25:38 +0500

    file loader moved to base webpack configs and stage-0 and es2015 babel presets removed

--- webpack.base.js
+++ webpack.base.js
@@ -9,6 +9,26 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel-loader",
       },
+      {
+        test: /\.(png|svg|jpg|gif|svg)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "img",
+          },
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "fonts",
+          },
+        },
+      },
     ],
   },
   plugins: [new WebpackCleanupPlugin(), new LoadablePlugin()],
