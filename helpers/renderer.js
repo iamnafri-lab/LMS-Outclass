30a8c1c4b033accb2d78646a681367bbbda16d0d
Usama Shehzad <usamashzd99@gmail.com>
2020-09-18 13:24:04 +0500

    ssr lazy loading bug fully resolved

--- helpers/renderer.js
+++ helpers/renderer.js
@@ -1,42 +1,51 @@
-import path from "path"
-import React from 'react';
-import { renderToString } from 'react-dom/server';
+import path from "path";
+import React from "react";
+import { renderToString } from "react-dom/server";
 
-import { StaticRouter } from "react-router-dom"
-import Routes from "../client/Routes"
-import { renderRoutes } from "react-router-config"
+import { StaticRouter } from "react-router-dom";
+import Routes from "../client/Routes";
+import { renderRoutes } from "react-router-config";
 
-import { Provider } from 'react-redux';
-import serialize from "serialize-javascript"
+import { Provider } from "react-redux";
+import serialize from "serialize-javascript";
 
-import { ChunkExtractor } from '@loadable/server'
+import { ChunkExtractor } from "@loadable/server";
 
-import { readFileAsync } from "./file"
+import { readFileAsync } from "./file";
 export default async (req, store, context) => {
+  const statsFile = path.resolve("./dist/loadable-stats.json");
+  const extractor = new ChunkExtractor({ statsFile });
 
-    const statsFile = path.resolve('./build/loadable-stats.json');
-    const extractor = new ChunkExtractor({ statsFile })
+  const content = renderToString(
+    extractor.collectChunks(
+      <Provider store={store}>
+        <StaticRouter location={req.path} context={context}>
+          {renderRoutes(Routes)}
+        </StaticRouter>
+      </Provider>
+    )
+  );
 
-    const content = renderToString(
-        extractor.collectChunks(
-            <Provider store={store}>
-                <StaticRouter location={req.path} context={context}>
-                    {renderRoutes(Routes)}
-                </StaticRouter>
-            </Provider>
-        )
-    );
+  const storeRehydrationState = serialize(store.getState());
 
-    const storeRehydrationState = serialize(store.getState());
+  const scriptTags = extractor.getScriptTags();
 
+  let bootupHTML = await readFileAsync("./dist/bootup.html");
 
+  bootupHTML = bootupHTML.replace(
+    /<script[^>]*>(?:(?!<\/script>)[^])*<\/script>/g,
+    ""
+  );
+  bootupHTML = bootupHTML.replace(
+    "<!-- __STORE_REHYDRATION_STATE__ -->",
+    `<script>window.__STORE_REHYDRATION_STATE__ = ${storeRehydrationState};</script>
+    ${scriptTags}`
+  );
 
-    let bootupHTML = await readFileAsync("./dist/bootup.html");
+  bootupHTML = bootupHTML.replace(
+    '<div id="root"></div>',
+    `<div id="root">${content}</div>`
+  );
 
-    bootupHTML = bootupHTML.replace("<!-- __STORE_REHYDRATION_STATE__ -->", `<script>window.__STORE_REHYDRATION_STATE__ = ${storeRehydrationState};</script>`);
-
-    bootupHTML = bootupHTML.replace("<div id=\"root\"></div>", `<div id="root">${content}</div>`);
-
-    return bootupHTML;
-
-}
+  return bootupHTML;
+};
