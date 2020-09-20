f1c1c35c5151130ecc019a04b187685ab78fc993
Usama Shehzad <usamashzd99@gmail.com>
2020-09-20 12:46:17 +0500

    css and scss files ignored in webpack bundle

--- webpack.server.js
+++ webpack.server.js
@@ -3,18 +3,19 @@ const webpackNodeExternals = require("webpack-node-externals");
 const { merge } = require("webpack-merge");
 const baseConfig = require("./webpack.base");
 
-
 const config = {
   target: "node",
-  mode: 'development',
+  mode: "development",
   entry: "./app.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build"),
   },
   externals: [webpackNodeExternals()],
+  module: {
+    rules: [{ test: /\.(scss|css)$/, loader: "ignore-loader" }],
+  },
 };
-
 
 let mergedConfigs = merge(baseConfig, config);
 
